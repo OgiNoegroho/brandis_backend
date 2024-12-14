a34d9d0dd49184e716139311d3102f685b73207a
OgiNoegroho <oginugroho512@gmail.com>
2024-12-14 18:33:13 +0700

    build: vercel deployment test

--- src/middleware/upload.middleware.ts
+++ src/middleware/upload.middleware.ts
@@ -1,17 +1,25 @@
-// src/middleware/auth.ts
+// src/middleware/upload.middleware.ts
 
 import multer from 'multer';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import { CloudinaryStorage, Options } from 'multer-storage-cloudinary';
 import cloudinary from '../config/cloudinary';
+
+// TypeScript Type Override
+interface CloudinaryStorageWithFolder extends Options {
+  params: Options['params'] & {
+    folder?: string;
+  };
+}
 
 // Configure Multer to use Cloudinary
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
-    folder: 'products', // The folder name in Cloudinary
-    allowed_formats: ['jpg', 'png', 'jpeg'], // Allowed image formats
-    transformation: [{ width: 500, height: 500, crop: 'limit' }], // Optional transformation
+    resource_type: 'auto',
+    allowed_formats: ['jpg', 'png', 'jpeg'],
+    transformation: [{ width: 500, height: 500, crop: 'limit' }],
   },
-});
+  folder: 'products' as string, // Override to specify folder
+} as CloudinaryStorageWithFolder);
 
 export const upload = multer({ storage });
